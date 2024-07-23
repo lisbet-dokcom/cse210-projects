ab55e856c07d70e5dbd718413e77881aae4f6adc
lisbet-dokcom <lissbetjb13@gmail.com>
2024-07-23 07:02:49 +0100

    commit

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Drawing;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square squ = new Square("Red", 7);
+        shapes.Add(squ);
+        Rectangle rec = new Rectangle("Blue", 7, 9);
+        shapes.Add(rec);
+        Circle cir = new Circle("Pink", 8);
+        shapes.Add(cir);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"This {color} shape has an area of {area}.");
+        }
+
     }
 }
